b76f6fddf09886949a8a8f80729e13c015e34e26
Mateus Luciano <teeusdm@gmail.com>
2021-04-26 20:17:19 -0300

    created header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import './styles.module.scss';
+
 export function Header() {
   return(
-    <header>
+    <header className="headerContainer">
       <img src="/logo.svg" alt="Podcastr" />
       <p>O melhor para você ouvir, sempre</p>
       <span>Qui, 8 Abril</span>
