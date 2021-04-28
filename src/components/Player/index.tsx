4d7ab259cb93e727ab47c2a033a1a1ce3433d528
Mateus Luciano <teeusdm@gmail.com>
2021-04-27 22:05:35 -0300

    updated player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,8 +1,11 @@
-import { useRef, useEffect } from 'react';
-import Image from 'next/image';
-import Slider from 'rc-slider';
+import { useRef, useEffect, useState } from 'react';
 
+import Image from 'next/image';
+
+import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import { usePlayer } from '../../contexts/PlayerContext';
 
@@ -10,6 +13,15 @@ import styles from './styles.module.scss';
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [progress, setProgress] = useState(0);
+
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0;
+    
+    audioRef.current.addEventListener('timeupdate', () => {
+      setProgress(Math.floor(audioRef.current.currentTime));
+    })
+  }
 
   const { 
     episodeList, 
@@ -65,10 +77,12 @@ export function Player() {
         ) }
       <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
-          <span>00</span>
+          <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             { episode ? (
               <Slider
+                max={episode.duration}
+                value={progress}
                 trackStyle={{ backgroundColor: '#04d361' }}
                 railStyle={{ backgroundColor: '#9f75ff' }}
                 handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
@@ -77,7 +91,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             ) }
           </div>
-          <span>00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
         { episode && (
           <audio 
@@ -87,6 +101,7 @@ export function Player() {
             autoPlay
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
+            onLoadedMetadata={setupProgressListener}
           />
         ) }
         <div className={styles.buttons}>
